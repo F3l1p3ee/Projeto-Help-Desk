ed8932bef3a2d86a37b945226f16f80c21f1bed4
Felipe da Silva Rodrigues <felipedasr3232@gmail.com>
2023-12-26 17:42:38 -0300

    Adicionando autenticação com dois usuários mockados

--- valida_login.php
+++ valida_login.php
@@ -1,8 +1,24 @@
 <?php 
-    $dados = [
-        "email" => $_POST["email"],
-        "senha" => $_POST["senha"]
+// Usuários do sistema
+    $usuarios_app = [
+        ["email" => "adm@teste.com", "senha" => "123456"],
+        ["email" => "user@teste.com", "senha" => "abcd"]
     ];
 
-    echo $dados["email"] . " & " . $dados["senha"];
+// Variável que verifica se a autenticação foi realizada    
+    $usuario_autenticado = false;
+
+// Validação de dados
+    foreach($usuarios_app as $user){
+        if($user["email"] === $_POST["email"] && $user["senha"] === $_POST["senha"]){
+            $usuario_autenticado = true;
+        }
+    }
+
+// Redirecionamento caso der OK
+    if($usuario_autenticado){
+        header("Location: home.php");
+    } else {
+        header("Location: index.php?login=erro");
+    }
 ?>
